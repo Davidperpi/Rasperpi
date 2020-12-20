74113e8934020a2b5c9fd361b30b7448788fe629
David <davidperpi7@gmail.com>
2020-12-20 19:28:31 +0100

    arreglos

--- server/config/config.js
+++ server/config/config.js
@@ -9,20 +9,9 @@ process.env.PORT = process.env.PORT || 3000; //
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // 
 
 // ===================
-// Vencimiento del Token
-// =================== 
-process.env.CADUCIDAD_TOKEN = { expiresIn: '120h' };
-
-// ===================
-// SEED
-// =================== 
-process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'; // 
-
-// ===================
 // Base de datos
 // =================== 
 let urlDB;
-
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/proyecto';
@@ -30,8 +19,3 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
-
-// ===================
-// Google Client ID
-// =================== 
-process.env.CLIENT_ID = process.env.CLIENT_ID || '44493566020-e7qftufef0j8100ffpv3eqmbntfqjcmg.apps.googleusercontent.com';
