f42dd705677012083f2f49508fd45d5b126e2e33
David <davidperpi7@gmail.com>
2020-12-20 19:53:29 +0100

    config bbdd

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/proyecto';
 } else {
-    urlDB = process.env.MONGO_URI;
+    // urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://admin:ea5Xtmrpxk8xEjKn@cluster0.jgfdg.mongodb.net/proyecto';
 }
 process.env.URLDB = urlDB;
